484b951eab247c9442f748b257bafa6c7b54715f
yuefengsu <1171890071@qq.com>
2020-04-25 13:58:49 +0800

    tes

--- y/a.js
+++ y/a.js
@@ -3,3 +3,24 @@ let b ='sdsd'
 
 let a = 'yu'
 
+git clone 
+
+git pull origin
+
+git add .
+
+git commit -m
+
+git push origin
+
+git branch
+
+git checkout
+
+git merge
+
+git branck -d 
+
+git push origin delete 
+
+git status
