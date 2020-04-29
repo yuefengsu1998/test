b413d8cf6623015d728093188ef5ca247e068325
yuefengsu1998 <39430455+yuefengsu1998@users.noreply.github.com>
2020-04-29 11:00:34 +0800

    Update a.js

--- y/a.js
+++ y/a.js
@@ -1,30 +1,30 @@
 
-let b ='sdsd'
+git clone '地址' //拉取远程仺库
 
-let a = 'yu'
+git pull origin '分支名' //拉取某个分支的代码
 
-git clone '地址'
+git add . //提交所有代码到缓存区（包括所有新添加的文件/文件夹）
 
-git pull origin '分支名'
+git commit -m 'xxxx' //提交代码到缓冲区
 
-git add .
+git push origin '分支名' //提交代码到某个远程分支
 
-git commit -m 'xxxx'
+git branch '分支名' //新建分支
 
-git push origin '分支名'
+git branch -ar //查看远程的分支
 
-git branch '分支名'
+git branch -f //查看本地的分支
 
-git branch -a
+git rm --cached '文件名' //删除缓存区的文件
 
-git branch -f
+git rm -r --cached '文件名' //删除缓存区的文件下的所有文件
 
-git checkout '分支名'
+git checkout '分支名' //切换分支
 
-git merge '分支名'
+git merge '分支名' //合拼分支
 
-git branck -d '分支名'
+git branck -d '分支名' //删除分支本地
 
-git push origin delete '分支名'
+git push origin delete '分支名' //删除远程分支
 
-git status
+git status //查看状态
